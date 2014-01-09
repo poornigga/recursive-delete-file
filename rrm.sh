f8c7a7890f1b115f836eebf745228ebabb97629a
王大狗 <zhainx@gmail.com>
2014-01-09 13:28:51 +0800

    colorful tips

--- rrm.sh
+++ rrm.sh
@@ -1,17 +1,17 @@
 #!/bin/bash - 
 #===============================================================================
 #
-#          FILE: rrm.sh 
+#          FILE:  rrm 
 # 
-#         USAGE:  rrm.sh foo
+#         USAGE:  ./rrm.sh foo
 # 
-#   DESCRIPTION:  eg. rrm.sh .svn  // delete all .svn folders recursively.
+#   DESCRIPTION:  e.g. ./rdf.sh .svn  // delete all .svn folders recursive.
 # 
 #       OPTIONS:  ---
 #  REQUIREMENTS:  ---
 #          BUGS:  ---
 #         NOTES:  ---
-#        AUTHOR:  bigdog()
+#        AUTHOR:  bigdog() 
 #       COMPANY: 
 #       CREATED: 2012/06/07 15时52分04秒 CST
 #      REVISION:  ---
@@ -27,8 +27,8 @@ CHECK_MARK="\xe2\x9c\x93"
 function usage () {
     echo -ne "\n${COLOR_BRIGHT_WHITE}rdf.sh, a small tool, be used for recursivly delete specified file or folder.${COLOR_NORMAL}\n"
     echo -ne "\nUsage :\n"
-    echo -ne "\t `basename $0` filename [ or foldername ]\n"
-    echo -ne "\n\t `basename $0` .svn   < will be delete all .svn folder> \n\n"
+    echo -ne "\t${COLOR_BOLD_RED} `basename $0`${COLOR_GREEN} filename [ or foldername ]${COLOR_NORMAL}\n"
+    echo -ne "\n\t ${COLOR_BOLD_RED}`basename $0` ${COLOR_GREEN}.svn ${COLOR_NORMAL}  < will be delete all .svn folder> \n\n"
 }
 
 if [ $# -lt 1 ]; then
@@ -40,11 +40,19 @@ echo -e "\n${COLOR_BOLD_RED}+---------------------------------------------------
 find ./ -name "$1" | grep "$1$"
 echo -e "${COLOR_BOLD_RED}+------------------------------------------------------------------+${COLOR_NORMAL}\n"
 
-echo -ne "\nThe files listed above will be delete.\n\n"
+count=`find ./ -name "$1" | grep "$1$" | wc -l`
+if [ ${count} -eq 0 ]; then
+    echo -e "${COLOR_BRIGHT_WHITE}\tNo file will be delete\n"
+    echo -e "\n${COLOR_GREEN}Exit${COLOR_NORMAL}\n"
+    exit 0
+fi
+
+echo -ne "\nThe ["$count" ] files listed above will be delete.\n\n"
 
 read -p "Continue ? [Y/N] : " answer
 
 if [ $answer != "y" -a $answer != "Y" ]; then
+    echo -e "\n${COLOR_BOLD_RED}Action cancelled."
     echo -e "\n${COLOR_GREEN}Exit${COLOR_NORMAL}\n"
     exit 0
 fi
@@ -52,5 +60,5 @@ fi
 find ./ -name "$1" | grep "$1$" | sed -e 's/^/rm -rf /g' | /bin/bash
 
 echo -e "\n${COLOR_GREEN}Success ${COLOR_BOLD_RED} ${CHECK_MARK} ${COLOR_NORMAL}\n"
-
 exit 0
+
